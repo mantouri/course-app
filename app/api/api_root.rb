35252ffa43f34a7f1f9563f62def8dda73211df3
ricky <ricky@jvdiamondtech.com>
2022-09-06 17:53:43 +0800

    加上 CORS 跨域訪問

--- app/api/api_root.rb
+++ app/api/api_root.rb
@@ -1,5 +1,11 @@
 # 為所有的 API endpoint 掛載入口點，可設置全局通用屬性
 class ApiRoot < Grape::API
+
+  # CORS 跨域訪問
+  # before do
+  #   header "Access-Control-Allow-Origin", "*"
+  # end
+
   PREFIX = '/api'.freeze
 
   format :json # 一般來說我們傳遞資料的格式都會用 json，如果沒有設定就會用 xml，所以這邊要設定 format 為 json
