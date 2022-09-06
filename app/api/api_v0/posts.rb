2eacb31b32c6793835fff6c7576d18ed0ea5773b
ricky <ricky@jvdiamondtech.com>
2022-09-06 16:35:10 +0800

    要能建立文章

--- app/api/api_v0/posts.rb
+++ app/api/api_v0/posts.rb
@@ -7,5 +7,28 @@ module ApiV0
       posts = current_user.posts
       present posts, with: ApiV0::Entities::Post
     end
+
+    desc "Create new post"
+    params do
+      requires :title, type: String
+      requires :context, type: String
+    end
+    post "/posts" do
+      # 這邊會用 declared 去濾 params 是因為這時候的設計 access_key 是放在 params 裡面，如果跟著帶進# 來會噴 error，因為 Post 這個 model 裡面並沒有 access_key 這個欄位會造成錯誤。
+      post = current_user.posts.new(declared(params, include_missing: false).except(:access_key))
+
+      if post.save
+        present post, with: ApiV0::Entities::Post
+      else
+        raise StandardError, $!
+      end
+    end
+
+    # 因為 grape 不見得能直接使用 rails strong param，通常帶進來的參數不會像是 form 送出去的會包很多層 hash
+    # rails request => { posts: { title: "Hi", context: "Man" } }
+    # rails controller can use => params.required(:posts).permit(:title, :context)
+    # 但通常 API 送進來的都是單層的
+    # api request => { title: "Hi", context: "Man" }
+    # 這裡才會用 grape 提供的 declared 去做處理 declared(params, include_missing:....)
   end
 end
